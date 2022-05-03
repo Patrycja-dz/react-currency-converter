ce3666abdca427805e9d6531aeb6d5bb49e15dd6
Patrycja-dz <dziemianko.pat@gmail.com>
2022-05-03 12:11:28 +0200

    created HeaderComponent with name and logo inside

---
+++ currency-converter-with-react/src/components/HeaderComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+ function HeaderComponent() {
+    return (
+    
+        <header className="converter__header">
+            <div className="converter__logo"></div>
+            <h1 className="converter__heading">Przelicznik walut</h1>
+        </header>
+        
+    );
+}
+
+export default HeaderComponent;
