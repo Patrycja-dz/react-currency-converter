9f96c0125e834f823e9937a7b3a00d4db3bf512e
Patrycja-dz <dziemianko.pat@gmail.com>
2022-05-04 12:52:52 +0200

    taking 3 currencies from api

--- currency-converter-with-react/src/App.js
+++ currency-converter-with-react/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import HeaderComponent from './components/HeaderComponent';
 import FormComponent from './components/FormComponent';
 import './App.css';
+
+const API = 'https://api.nbp.pl/api/exchangerates/tables/a/?format=json';
+
 function App() {
+  const [currencyOptions, setCurrency] = useState([])
+  console.log(currencyOptions)
+  useEffect(()=>{
+    fetch(API)
+    .then(res => res.json())
+    .then(data => {
+      const currencyElements = data[0].rates.filter((rate) => rate.code === "USD" || rate.code === "EUR" || rate.code === "CHF")
+      setCurrency(currencyElements)
+      console.log(setCurrency)
+    })
+    
+  },[])
   return (
  <>
       <HeaderComponent/>
-      <FormComponent/>
+      <FormComponent 
+      currencyOptions = {currencyOptions}/>
       <p className='converter__result js-result'></p>
  </>
   );
