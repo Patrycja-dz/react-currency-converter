0b07b27f3cde3be87e0dcdffd478261d7655c7b9
Patrycja-dz <dziemianko.pat@gmail.com>
2022-05-09 10:47:05 +0200

    generate three values from api

--- currency-converter-with-react/src/App.js
+++ currency-converter-with-react/src/App.js
@@ -1,29 +1,16 @@
-import React, {useEffect,useState} from 'react';
+import React from 'react';
 import HeaderComponent from './components/HeaderComponent';
 import FormComponent from './components/FormComponent';
 import './App.css';
 
-const API = 'https://api.nbp.pl/api/exchangerates/tables/a/?format=json';
 
-function App() {
-  const [currencyOptions, setCurrency] = useState([])
-  console.log(currencyOptions)
-  useEffect(()=>{
-    fetch(API)
-    .then(res => res.json())
-    .then(data => {
-      const currencyElements = data[0].rates.filter((rate) => rate.code === "USD" || rate.code === "EUR" || rate.code === "CHF")
-      setCurrency(currencyElements)
-      console.log(setCurrency)
-    })
-    
-  },[])
+
+function App(props) { 
   return (
  <>
       <HeaderComponent/>
-      <FormComponent 
-      currencyOptions = {currencyOptions}/>
-      <p className='converter__result js-result'></p>
+      <FormComponent/>
+   
  </>
   );
 }
--- currency-converter-with-react/src/components/FormComponent.js
+++ currency-converter-with-react/src/components/FormComponent.js
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 
- function FormComponent(props) {
-     const{
-         currencyOptions
-     } = props
+const API = 'https://api.nbp.pl/api/exchangerates/tables/a/?format=json';
+ function FormComponent() {
+     const [currency,setCurrencies] = useState([])
+     const [result,setResult] = useState(0)
+    useEffect(() => {
+        fetch(API)
+          .then((response) => response.json())
+         .then((data) => {
+            let currencies = data[0].rates;
+                const currencyElements = currencies.filter((rate) => rate.code === "USD" || rate.code === "EUR" || rate.code === "CHF");
+                setCurrencies(currencyElements);
+              })
+              .catch(console.log);
+          }, []);
+          const calculateCurrency = (e) => {
+              e.preventDefault()
+             const {number,chosenCurrency} = e.target.elements
+             const amountValue = number.value;
+             const currencyValue = currency.find(
+      (rate) => rate.code === chosenCurrency.value
+    )?.mid;
+    const calculatedTotal = (amountValue * currencyValue).toFixed(2);
+
+    setResult(calculatedTotal);
+          };
+        
     return (
-        <form className="converter__inputs js-form">
+        <form className="converter__inputs js-form" onSubmit={calculateCurrency}>
         <div className="converter__input">
             <input type="number" name="number" step="0.01" id="js-number" className="converter__number js-input"
                 placeholder="Wpisz kwotę w zł"/>
         </div>
         <div className="converter__input">
-            <select name="select" id="js-select" className="converter__select js-currencies">
-                {currencyOptions.map(option => (
-                    <option key={option.code}>{option.code}</option>
-                ))}
-                <option value="">Wybierz walutę</option>
+            <select id="js-select" className="converter__select js-currencies" name="chosenCurrency">
+                 <option value="">Wybierz walutę</option>
+                 <option value="EUR">EUR</option>
+                 <option value="USD">USD</option>
+                 <option value="CHF">CHF</option>
             </select>
         </div>
         <button className="js-button converter__button">Przelicz</button>
-        <button className="js-reset converter__button">Resetuj</button>
+        <p className ="converter__result"><span>To:</span> {result} <span>zł</span></p>
     </form>    
     );
 }
